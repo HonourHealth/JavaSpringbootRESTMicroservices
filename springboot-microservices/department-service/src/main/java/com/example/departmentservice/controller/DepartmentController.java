203b9b317a740c13b56aa6f6e016638b2111a718
Onur Can <24491805+HonourHealth@users.noreply.github.com>
2025-01-21 18:44:40 +0300

    added department and employee contollers, dtos, repositories, services, entities and relevant configuration

---
+++ springboot-microservices/department-service/src/main/java/com/example/departmentservice/controller/DepartmentController.java
@@ -0,0 +1,30 @@
+package com.example.departmentservice.controller;
+
+import com.example.departmentservice.dto.DepartmentDto;
+import com.example.departmentservice.service.DepartmentService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/departments")
+public class DepartmentController {
+
+    private DepartmentService departmentService;
+
+    @PostMapping
+    public ResponseEntity<DepartmentDto> saveDepartment(@RequestBody DepartmentDto departmentDto) {
+        DepartmentDto savedDepatment = departmentService.saveDepartment(departmentDto);
+        return new ResponseEntity<>(savedDepatment, HttpStatus.CREATED);
+    }
+
+    @GetMapping("{department-code}")
+    public ResponseEntity<DepartmentDto> getDepartment(@PathVariable("department-code") String departmentCode) {
+        DepartmentDto departmentByCode = departmentService.getDepartmentByCode(departmentCode);
+        return ResponseEntity.ok(departmentByCode);
+    }
+
+
+}
