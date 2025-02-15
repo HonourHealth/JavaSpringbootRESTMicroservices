c1dc64e40d35c795d2e13cc065217b18e443e72e
Onur Can <24491805+HonourHealth@users.noreply.github.com>
2025-02-15 14:25:54 +0300

    add Kafka Wikimedia project

---
+++ springboot-kafka-wikimedia-project/src/test/java/com/example/springbootkafkawikimedia/SpringbootKafkaWikimediaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootkafkawikimedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaWikimediaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
