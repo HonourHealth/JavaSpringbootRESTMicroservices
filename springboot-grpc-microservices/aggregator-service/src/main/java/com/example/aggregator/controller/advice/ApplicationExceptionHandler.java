c22574ce578b9f9f6c8097804dff92cf0b2b96e8
Onur Can <24491805+HonourHealth@users.noreply.github.com>
2025-07-09 17:09:23 +0300

    Add exception handling for gRPC StatusRuntimeException in ApplicationExceptionHandler

--- springboot-grpc-microservices/aggregator-service/src/main/java/com/example/aggregator/controller/advice/ApplicationExceptionHandler.java
+++ springboot-grpc-microservices/aggregator-service/src/main/java/com/example/aggregator/controller/advice/ApplicationExceptionHandler.java
@@ -3,9 +3,12 @@ package com.example.aggregator.controller.advice;
 import io.grpc.StatusRuntimeException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ApplicationExceptionHandler {
+
+    @ExceptionHandler(StatusRuntimeException.class)
     public ResponseEntity<String> handleStatusRuntimeException(StatusRuntimeException e) {
         return switch (e.getStatus().getCode()) {
             case INVALID_ARGUMENT, FAILED_PRECONDITION ->
