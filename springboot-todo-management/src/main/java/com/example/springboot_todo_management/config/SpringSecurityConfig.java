11cd349ff8d05540eb929ca3258e793a293138d9
Onur Can <24491805+HonourHealth@users.noreply.github.com>
2025-07-28 21:00:43 +0300

    feat: enhance Spring Security configuration with role-based access control; refactor role management to use enum

--- springboot-todo-management/src/main/java/com/example/springboot_todo_management/config/SpringSecurityConfig.java
+++ springboot-todo-management/src/main/java/com/example/springboot_todo_management/config/SpringSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.springboot_todo_management.config;
 
+import lombok.Getter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,17 +12,44 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 
 @Configuration
 public class SpringSecurityConfig {
 
+    @Bean
     public static PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    @Getter
+    private enum Role {
+        ADMIN("ADMIN"),
+        USER("USER");
+
+        private final String value;
+
+        Role(String value) {
+            this.value = value;
+        }
+
+    }
+
+    private static final String URL = "/api/**";
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable); //http.csrf((csrf) -> csrf.disable())
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers(HttpMethod.POST, URL).hasRole(Role.ADMIN.getValue());
+                    authorize.requestMatchers(HttpMethod.PUT, URL).hasRole(Role.ADMIN.getValue());
+                    authorize.requestMatchers(HttpMethod.DELETE, URL).hasRole(Role.ADMIN.getValue());
+                    authorize.requestMatchers(HttpMethod.GET, URL).hasAnyRole(Role.ADMIN.getValue(), Role.USER.getValue());
+                    authorize.requestMatchers(HttpMethod.PATCH, URL).hasAnyRole(Role.ADMIN.getValue(), Role.USER.getValue());
+                    authorize.anyRequest().authenticated();
+                })
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
@@ -30,12 +58,12 @@ public class SpringSecurityConfig {
         UserDetails user1 = User.builder()
                 .username("user1")
                 .password(passwordEncoder().encode("password1"))
-                .roles("USER")
+                .roles(Role.USER.getValue())
                 .build();
         UserDetails admin = User.builder()
                 .username("admin")
                 .password(passwordEncoder().encode("admin"))
-                .roles("ADMIN")
+                .roles(Role.ADMIN.getValue())
                 .build();
 
         return new InMemoryUserDetailsManager(user1, admin);
