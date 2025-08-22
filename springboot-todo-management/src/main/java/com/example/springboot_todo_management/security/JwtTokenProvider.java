4eb97cbbcb625291dcfab74ed719360176610911
Onur Can <24491805+HonourHealth@users.noreply.github.com>
2025-08-22 20:13:21 +0300

    feat: enhance JWT token management; add methods for token generation, username extraction, and validation in JwtTokenProvider, and configure JWT secret and expiration properties

--- springboot-todo-management/src/main/java/com/example/springboot_todo_management/security/JwtTokenProvider.java
+++ springboot-todo-management/src/main/java/com/example/springboot_todo_management/security/JwtTokenProvider.java
@@ -1,4 +1,55 @@
 package com.example.springboot_todo_management.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
 public class JwtTokenProvider {
+    @Value("${app.jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${app.jwt.expiration-milliseconds}")
+    private long jwtExpirationDate;
+
+    public String generateToken(Authentication authentication) {
+        String user = authentication.getName();
+
+        Date currentDate = new Date();
+
+        Date expireDate = new Date(currentDate.getTime() + jwtExpirationDate);
+
+        return Jwts.builder()
+                .setSubject(user)
+                .setIssuedAt(new Date())
+                .setExpiration(expireDate)
+                .signWith(key())
+                .compact();
+    }
+
+    private Key key() {
+        return Keys.hmacShaKeyFor(
+                Decoders.BASE64.decode(jwtSecret)
+        );
+    }
+
+    public String getUsername(String token) {
+        return Jwts.parser()
+                .setSigningKey(key())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        Jwts.parser().setSigningKey(key()).build().parse(token);
+        return true;
+    }
 }
