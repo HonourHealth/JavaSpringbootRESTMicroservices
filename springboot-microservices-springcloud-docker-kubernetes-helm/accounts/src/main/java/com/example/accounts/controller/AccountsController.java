5b7f620ce0900ae227fc2c5b59fc0c6bc1630f18
Onur Can <24491805+HonourHealth@users.noreply.github.com>
2026-01-03 17:28:23 +0300

    feat: add rate limiting and fallback method for Java version endpoint; configure Redis connection settings

--- springboot-microservices-springcloud-docker-kubernetes-helm/accounts/src/main/java/com/example/accounts/controller/AccountsController.java
+++ springboot-microservices-springcloud-docker-kubernetes-helm/accounts/src/main/java/com/example/accounts/controller/AccountsController.java
@@ -6,6 +6,7 @@ import com.example.accounts.dto.CustomerDto;
 import com.example.accounts.dto.ErrorResponseDto;
 import com.example.accounts.dto.ResponseDto;
 import com.example.accounts.service.IAccountsService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -146,11 +147,18 @@ public class AccountsController {
                     schema = @Schema(implementation = ErrorResponseDto.class)
             )
     )
+    @RateLimiter(name = "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion() {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(environment.getProperty("JAVA_HOME"));
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Java 21.0.0-FALLBACK");
     }
 
     @Operation(
